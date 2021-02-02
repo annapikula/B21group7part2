a3296e58fc77e69a042b67b3a2ae29577b3a445e
annapikula <74196770+annapikula@users.noreply.github.com>
2021-02-01 19:03:28 -0500

    Common files added

--- src/test/java/com/trycloud/utilities/BrowserUtils.java
+++ src/test/java/com/trycloud/utilities/BrowserUtils.java
@@ -1,4 +1,14 @@
 package com.trycloud.utilities;
 
 public class BrowserUtils {
+
+    public static void sleep(int second) {
+        second *= 1000;
+        try {
+            Thread.sleep(second);
+        } catch (InterruptedException e) {
+            System.out.println("something happened in sleep method");
+
+        }
+    }
 }
