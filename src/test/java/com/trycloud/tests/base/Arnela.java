0df59cde835495101027118897fc3ab9c77280b8
Arnela <arnellamiloss@gmail.com>
2021-02-04 11:29:30 -0500

    said Hello everyone

--- src/test/java/com/trycloud/tests/base/Arnela.java
+++ src/test/java/com/trycloud/tests/base/Arnela.java
@@ -2,6 +2,6 @@ package com.trycloud.tests.base;
 
 public class Arnela {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello everyone");
     }
 }
